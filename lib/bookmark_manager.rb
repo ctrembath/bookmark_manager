de3b25d01a5029139ae619bf0557b4e9eb6d8e98
ctrembath <claretrembath@ymail.com>
2014-11-18 11:05:06 +0000

    added tag links

--- lib/bookmark_manager.rb
+++ lib/bookmark_manager.rb
@@ -35,3 +35,9 @@ set :views, Proc.new { File.join(root, '..', 'views') }
     redirect to('/')
   end
 
+  get '/tags/:text' do
+    tag = Tag.first(:text => params[:text])
+    @links = tag ? tag.links : []
+    erb :index
+  end
+
