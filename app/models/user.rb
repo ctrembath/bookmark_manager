b26407477fb52cf263da67d9f96b7a27f880ad03
ctrembath <claretrembath@ymail.com>
2014-12-02 09:35:17 +0000

    added api key, but still send message method not working correctly

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,13 +36,13 @@ class User
     end
   end
 
-  def send_message(email, token)
-    RestClient.post "https://api:key-1a15cf6167de1edd52ebcecc5ae6fce0",
-    "@api.mailgun.net/v2/v2/app31831016.mailgun.org",
-    :from => "Mailgun Sandbox <postmaster@sandbox688a59fc15ff4cd0ac6f9c4e95d70c39.mailgun.org>",
-    :to => email,
-    :subject => "Mailgun test",
-    :text => "Please follow this link to change your password, the link is only available for one hour: #{BOOKMARK_URL}/users/change_password/#{token}."
-  end
+  # def send_message(email, token)
+  #   RestClient.post "https://api:key-1a15cf6167de1edd52ebcecc5ae6fce0",
+  #   "@api.mailgun.net/v2/v2/app31831016.mailgun.org",
+  #   :from => "Mailgun Sandbox <postmaster@sandbox688a59fc15ff4cd0ac6f9c4e95d70c39.mailgun.org>",
+  #   :to => email,
+  #   :subject => "Mailgun test",
+  #   :text => "Please follow this link to change your password, the link is only available for one hour: #{BOOKMARK_URL}/users/change_password/#{token}."
+  # end
 
 end
