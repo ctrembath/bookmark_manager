5d703ca033426b51f079c3b0400c30b4c9b3870e
ctrembath <claretrembath@ymail.com>
2014-12-07 19:57:42 +0000

    trying to prevent duplicate email address sign up

--- spec/features/helpers/session.rb
+++ spec/features/helpers/session.rb
@@ -1,6 +1,7 @@
 module SessionHelpers
 
-   def sign_up(email = "alice@example.com", password = "oranges!", password_confirmation = "oranges!")      visit '/users/new'
+   def sign_up(email = "alice@example.com", password = "oranges!", password_confirmation = "oranges!")      
+    visit '/users/new'
     fill_in :email, :with => email
     fill_in :password, :with => password
     fill_in :password_confirmation, :with => password_confirmation
